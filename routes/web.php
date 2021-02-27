d0fcc2dcb349343db6eec0a4af8d3f05765c98df
marcopolello <marco-pol@hotmail.it>
2021-02-27 22:50:54 +0100

    accetta carta ma manca gestione backend

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,32 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// gruppo di rotte solo per utenti registrati
+Route::group([
+  'middleware' => 'auth',
+], function(){
+
+  // rotte x tutti gli order nel db
+  Route::get('/orders', 'Ordercontroller@index')
+    -> name('orders-index');
+  Route::get('/order/{id}', 'Ordercontroller@show')
+    -> name('order-show');
+  // create & storeB
+  Route::get('/new/order', 'OrderController@create')
+    -> name('order-create');
+  Route::post('/new/order/store', 'OrderController@store')
+    -> name('order-store');
+  // edit & update
+  Route::get('/edit/{id}', 'OrderController@edit')
+    -> name('order-edit');
+  Route::post('/update/{id}', 'OrderController@update')
+    -> name('order-update');
+    // delete
+  Route::get('/delete/{id}', 'OrderController@delete')
+    -> name('order-delete');
+
+
+
+
+});

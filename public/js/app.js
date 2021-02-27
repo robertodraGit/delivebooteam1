8983ea3d96b3f945ec1d51beeca1890b46167472
marcopolello <marco-pol@hotmail.it>
2021-02-27 13:03:06 +0100

    first commit marco

--- public/js/app.js
+++ public/js/app.js
@@ -50418,8 +50418,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\progetti-laravel\delivebooteam1\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\progetti-laravel\delivebooteam1\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\marco\Boolean\progetti-esercizi\delivebooteam1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\marco\Boolean\progetti-esercizi\delivebooteam1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

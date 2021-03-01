8e1e4ebffe84b1115ba182954ce4d000d937d467
robertodraGit <rdrago.uni@gmail.com>
2021-03-01 11:40:30 +0100

    new branch (img to fix in plate)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center mb-3">
+    <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
@@ -16,15 +16,8 @@
 
                     {{ __('You are logged in!') }}
                 </div>
-
             </div>
         </div>
     </div>
-
-    <!-- Load the required client component. -->
-  <script src="https://js.braintreegateway.com/web/3.73.1/js/client.min.js"></script>
-
-  <!-- Load Hosted Fields component. -->
-  <script src="https://js.braintreegateway.com/web/3.73.1/js/hosted-fields.min.js"></script>
-
+</div>
 @endsection

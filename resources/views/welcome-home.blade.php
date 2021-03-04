3818b180b2420561008c7c220476eb1bed509772
sourceCodeISA <isabel.gogov1@libero.it>
2021-03-04 23:13:18 +0100

    header ok

--- resources/views/welcome-home.blade.php
+++ resources/views/welcome-home.blade.php
@@ -2,50 +2,32 @@
 
 @section('content')
 
-  {{-- HEADER --}}
-    <header>
-        <div class="container">
-            <div class="menu"> <!-- vue -->
-                <a href="#">menu</a>
-            </div>
-            <div class="logo">logo</div>
-            <input type="text" placeholder="Piatti">
-            <div class="cart">cart</div>
-        </div>
-    </header>
-
     {{-- MAIN --}}
     <main>
 
         <div class="container">
+            
+          {{-- <div id="app">
+            <slider></slider>
+          </div> --}}
+          
 
-          <div class="plates_container">
-
-            <div id="app">
-
-              @foreach ($plates as $plate)
-                <plate
-                :plate_data = {{ $plate }}
-                >
-                </plate>
-              @endforeach
-
+          <section>
               <h1>Ristoranti</h1>
 
+              <div class="restaurants">
 
-                <restaurant
-                :restaurant_data = {{ $restaurants }}
-                >
-                </restaurant>
+                  <div class="card">info che arrivano dal db</div> <!-- foreach componente ristorante-->
+                  {{-- <div class="card">info</div>
+                  <div class="card">info</div>
+                  <div class="card">info</div>
+                  <div class="card">info</div>
+                  <div class="card">info</div> --}}
 
-
-            </div>
-
-
-          </div>
+              </div>
+          </section>
 
         </div>
-
 
     </main>
 
@@ -79,5 +61,4 @@
             <div class="copyright">copyright</div>
         </div>
     </footer>
-
 @endsection

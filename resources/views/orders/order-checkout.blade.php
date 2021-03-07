61913728c9888b976f42d7bf118dc3a4681d40f5
robertodraGit <rdrago.uni@gmail.com>
2021-03-07 22:37:47 +0100

    cart -> order working again

--- resources/views/orders/order-checkout.blade.php
+++ resources/views/orders/order-checkout.blade.php
@@ -9,20 +9,23 @@
 
       <div class="col-md-12">
 
-      @foreach ($data_array['plates'] as $item)
-        <ul>
-          <li>{{$item -> plate_name}}</li>
-          <li>{{$item -> price}}</li>
-        </ul>
-      @endforeach
-
-      Delivery cost: {{$data_array['delivery']}} <br> 
-      Total to pay: {{$data_array['topay'] + $data_array['delivery']}} <br>
+      
       
 
       <form action="{{ route('order-store') }}" method="post">
         @csrf
         @method('POST')
+
+        @foreach ($data_array['plates'] as $item)
+          <ul>
+            <li>{{$item -> plate_name}}</li>
+            <li>{{$item -> price}}</li>
+          </ul>
+        @endforeach
+
+        Delivery cost: {{$data_array['delivery']}} <br> 
+        Total to pay: {{$data_array['topay'] + $data_array['delivery']}} 
+        <br>
 
         @if ($errors->any())
           <div class="alert alert-danger">
@@ -34,7 +37,9 @@
           </div>
         @endif
 
-
+        @foreach ($data_array['plates'] as $key => $item)
+          <input type="hidden" name='plate_{{$key +1}}' value='{{$item}}'>
+        @endforeach
 
         <label for="first_name">first_name: </label>
         <input type="text" name="first_name" value="">

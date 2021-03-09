3c7ff744c3a6dce1203f60a034c706958d9976e0
sourceCodeISA <isabel.gogov1@libero.it>
2021-03-09 09:36:30 +0100

    modifiche su edit restaurant

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Mail\PayMail;
 use App\User;
 
-Route::get('/', 'Controller@index') -> name ('index');
+Route::get('/', 'Controller@index') -> name ('index'); 
 
 Auth::routes();
 

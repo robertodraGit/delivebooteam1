a9171db4436274d1349152940e7e6f79fedfc0a8
marcopolello <marco-pol@hotmail.it>
2021-03-11 18:20:54 +0100

    5 sec function msg

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,7 @@ function  init() {
          this.messageVisible = true,
          setTimeout(function(scope){
            scope.messageVisible = false;
-         }, 555000, this);
+         }, 5000, this);
        })},
       data: {
           restaurants: [],
